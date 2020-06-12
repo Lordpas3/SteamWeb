f64ff53c7ccc646de5edb667161c9164c3a04680
Lordpas3 <lordpas3@gmail.com>
2020-06-12 12:08:58 +0200

    changed CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let games = [
+  {
+    name: 'Terraria',
+    img: require('./img/Terraria.jpg'),
+    steamAppID: '105600'
+  },
+  {
+    name: 'Smite',
+    img: require('./img/Smite.jpg'),
+    steamAppID: '386360'
+  },
+  {
+    name: 'Europa Universalis 4',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  },
+  {
+    name: 'Placeholder text',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  }, {
+    name: 'Placeholder text',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  }, {
+    name: 'Placeholder text',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  }, {
+    name: 'Placeholder text',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  }, {
+    name: 'Placeholder text',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  }, {
+    name: 'Placeholder text',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  }, {
+    name: 'Placeholder text',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  }, {
+    name: 'Placeholder text',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  }, {
+    name: 'Placeholder text',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  }, {
+    name: 'Placeholder text',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  }, {
+    name: 'Placeholder text',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  }, {
+    name: 'Placeholder text',
+    img: require('./img/eu4.jpg'),
+    steamAppID: '236850'
+  }
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {games.map(game =>
+          <div className='App-margin'>
+            <p>{game.name}</p>
+            <a href={'steam://run/' + game.steamAppID}>
+              <img
+                src={game.img}
+                alt={game.name}
+                height='180px'
+                width='320px'
+              />
+            </a>
+          </div>)}
       </header>
-    </div>
+    </div >
   );
 }
+
 
 export default App;
